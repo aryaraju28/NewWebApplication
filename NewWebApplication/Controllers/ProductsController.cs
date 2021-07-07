ed70bf846630b8b562c86bfc4a15f48fc34dc3ae
aryaraju28 <arya28199@gmail.com>
2021-07-07 14:38:52 +0530

    Add project files.

---
+++ NewWebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewWebApplication.Models;
+using NewWebApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewWebApplication.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+      
+            public ProductsController(JsonFileProductService productService)
+            {
+                ProductService = productService;
+            }
+
+            public JsonFileProductService ProductService { get; }
+
+            [HttpGet]
+            public IEnumerable<Product> Get()
+            {
+                return ProductService.GetProducts();
+            }
+            //[HttpPatch]"[FromBody]"
+            [Route("Rate")]
+            [HttpGet]
+            public ActionResult Get(
+                [FromQuery] string ProductId,
+                [FromQuery] int Rating)
+            {
+                ProductService.AddRating(ProductId, Rating);
+
+                return Ok();
+            }
+
+
+        }
+    }
+
